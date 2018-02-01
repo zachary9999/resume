a8bcdba74d97b168e4a23f68408d199cb5a1db09
zachary9999 <34023070+zachary9999@users.noreply.github.com>
2018-02-01 14:14:02 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -89,9 +89,13 @@ var initS4 = function (bool) {
         $('#s4-1').css({'animationDelay':delayS + 's','animationDuration':'2s'}).addClass('fadeIn');
         $('#s4-2').css('animationDelay', delayS * 1.5 + 's').addClass('fadeInLeft');
         $('#s4-3').css('animationDelay', delayS * 1.5 + 's').addClass('ta');
+        $('#s4-4').css('animationDelay', delayS * 1.5 + 's').addClass('ta');
+        $('#s4-5').css('animationDelay', delayS * 1.5 + 's').addClass('ta');
     } else {
         $('#s4-1').removeClass('fadeIn');
         $('#s4-3').removeClass('ta');
+        $('#s4-4').removeClass('ta');
+        $('#s4-5').removeClass('ta');
     }
 }
 /**
@@ -117,91 +121,88 @@ function changeMenu(index) {
  * 3D作品集
  */
 function initPub() {
-    //3D相册
-    var oImgs = document.querySelectorAll('.img'),
-        nowX,
-        nowY,
-        lastX,
-        lastY,
-        minusX,
-        minusY,
-        roY = 0,
-        roX = -20;
-    var timer;
-    var oWrap = document.querySelector("#wrap");
-    // document.querySelectorAll('img') 会消耗性能
-    for (var i = 0; i < oImgs.length; i++) {
-        // debugger;
-        var ideg = 360 / 11 * i;
-        oImgs[i].style.transform = 'rotateY(' + ideg + 'deg) translateZ(350px)';
-        // debugger;
-        oImgs[oImgs.length - i - 1].style.transitionDelay = 0.2 * i + "s";
-    }
-
-    function getSin() {
-        var sideB = nowX - lastX;
-        var sideA = nowY - lastY;
-        var sideC = Math.sqrt(sideA * sideA + sideB * sideB);
-        var sinA = sideA / sideC;
-        console.log(sinA);
-    }
-
-    document.onmousedown = function (e) {
-        var e = e || window.event; //兼容IE与火狐
-        // console.log(e.clientX + "  " + e.clientY);
-        // debugger;
-        lastX = e.clientX;
-        lastY = e.clientY; //第一次执行的坐标
-
-        this.onmousemove = function (e) {
-            // debugger;
-            var e = e || window.event; //兼容IE与火狐
-            nowX = e.clientX;
-            nowY = e.clientY;
-            minusY = lastY - nowY; //坐标差
-            minusX = nowX - lastX;
-            roX += minusY * 0.002;
-            roY += minusX * 0.004;
-
-            if (getSin() > 0.7) {
-
-                oWrap.style.transform = "rotateX(" + roX + "deg)";
-            }
-            else {
-                oWrap.style.transform = "rotateY(" + roY + "deg)";
-            }
-            // oWrap.style.transform = "rotateX(" + roX + "deg) rotateY(" + roY + "deg)";
-
-
-        }
-        this.onmouseup = function () {
-
-            if (Math.abs(getSin()) > 0.7) {
-
-                oWrap.style.transform = "rotateX(" + roX + "deg)";
-            }
-            else {
-                oWrap.style.transform = "rotateY(" + roY + "deg)";
-            }
-            // debugger;
-
-
-            //惯性添加
-            this.onmousemove = null; //清除事件
-            timer = setInterval(function () {
-                minusY *= .98;
-                minusX *= .98; //速度递减
-
-                roX -= minusY * 0.02;
-                roY -= minusX * 0.01;
-
-
-                // oWrap.style.transform = "rotateX(" + roX + "deg) rotateY(" + roY + "deg)";
-                // oWrap.style.transform = "rotateY(" + roY + "deg)";
-                if (Math.abs(minusY) < .1 && Math.abs(minusX) < .1) {
-                    clearInterval(timer);
-                }
-            }, 13)
-        }// end mouse up
-    }
+    // //3D相册
+    // var oImgs = document.querySelectorAll('.img'),
+    //     nowX,
+    //     nowY,
+    //     lastX,
+    //     lastY,
+    //     minusX,
+    //     minusY,
+    //     roY = 0,
+    //     roX = -20;
+    // var timer;
+    // var oWrap = document.querySelector("#wrap");
+    // // document.querySelectorAll('img') 会消耗性能
+    // for (var i = 0; i < oImgs.length; i++) {
+    //     // debugger;
+    //     var ideg = 360 / 11 * i;
+    //     oImgs[i].style.transform = 'rotateY(' + ideg + 'deg) translateZ(350px)';
+    //     // debugger;
+    //     oImgs[oImgs.length - i - 1].style.transitionDelay = 0.2 * i + "s";
+    // }
+    //
+    // function getSin() {
+    //     var sideB = nowX - lastX;
+    //     var sideA = nowY - lastY;
+    //     var sideC = Math.sqrt(sideA * sideA + sideB * sideB);
+    //     var sinA = sideA / sideC;
+    //     console.log(sinA);
+    // }
+    //
+    // document.onmousedown = function (e) {
+    //     var e = e || window.event; //兼容IE与火狐
+    //     // console.log(e.clientX + "  " + e.clientY);
+    //     // debugger;
+    //     lastX = e.clientX;
+    //     lastY = e.clientY; //第一次执行的坐标
+    //
+    //     this.onmousemove = function (e) {
+    //         // debugger;
+    //         var e = e || window.event; //兼容IE与火狐
+    //         nowX = e.clientX;
+    //         nowY = e.clientY;
+    //         minusY = lastY - nowY; //坐标差
+    //         minusX = nowX - lastX;
+    //         roX += minusY * 0.002;
+    //         roY += minusX * 0.004;
+    //
+    //         if (getSin() > 0.7) {
+    //
+    //             oWrap.style.transform = "rotateX(" + roX + "deg)";
+    //         }
+    //         else {
+    //             oWrap.style.transform = "rotateY(" + roY + "deg)";
+    //         }
+    //         // oWrap.style.transform = "rotateX(" + roX + "deg) rotateY(" + roY + "deg)";
+    //
+    //
+    //     }
+    //     this.onmouseup = function () {
+    //
+    //         if (Math.abs(getSin()) > 0.7) {
+    //
+    //             oWrap.style.transform = "rotateX(" + roX + "deg)";
+    //         }
+    //         else {
+    //             oWrap.style.transform = "rotateY(" + roY + "deg)";
+    //         }
+    //         // debugger;
+    //
+    //
+    //         //惯性添加
+    //         this.onmousemove = null; //清除事件
+    //         timer = setInterval(function () {
+    //             minusY *= .98;
+    //             minusX *= .98; //速度递减
+    //             roX -= minusY * 0.02;
+    //             roY -= minusX * 0.01;
+    //             // oWrap.style.transform = "rotateX(" + roX + "deg) rotateY(" + roY + "deg)";
+    //             // oWrap.style.transform = "rotateY(" + roY + "deg)";
+    //             if (Math.abs(minusY) < .1 && Math.abs(minusX) < .1) {
+    //                 clearInterval(timer);
+    //             }
+    //         }, 13)
+    //     }// end mouse up
+    // }
 }
